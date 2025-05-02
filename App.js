ae6d306993038011aec08334f1f938a23ecccf49
swygen <swygenofficial@gmail.com>
2025-05-02 16:45:39 +0600

    Create App.js

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import LoadingScreen from './LoadingScreen';  // লোডিং স্ক্রিন কম্পোনেন্ট
+import Signup from './Signup';  // সাইন-আপ কম্পোনেন্ট
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/loading" exact component={LoadingScreen} />
+        <Route path="/signup" exact component={Signup} />
+        <Redirect from="/" to="/loading" />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
